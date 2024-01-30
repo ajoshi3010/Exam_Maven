3909731375f9706eda1068c6438e60bd6d05e52f
AnirudhJoshi <vuddawaranirudhjoshi03@gmail.com>
2024-01-30 12:38:31 +0530

    Initial commit

---
+++ src/main/java/person/App.java
@@ -0,0 +1,14 @@
+
+package person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
